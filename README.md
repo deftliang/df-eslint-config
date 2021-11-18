96d5b62ca16ae1d75198e74531ffe7553e038674
deft <deftmail@163.com>
2021-11-18 15:24:53 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # @deft/eslint-config
+
+### Start
+
+```js
+npm i eslint prettier @deft/eslint-config -D
+
+```
+
+```js
+yarn add eslint prettier @deft/eslint-config --dev
+
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+
+  parser: 'vue-eslint-parser',
+
+  // https://cn.eslint.org/docs/user-guide/configuring#specifying-parser-options
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-restricted-syntax': 'off',
+    'import/no-cycle': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'object-curly-newline': 'off',
+    'no-param-reassign': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'consistent-return': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
